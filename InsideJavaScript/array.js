967fa0c01a41ed74497226b5e6bdeb144ed34390
밀가루 <bbangaro@gmail.com>
2020-05-18 00:22:10 +0900

    Array

---
+++ InsideJavaScript/array.js
@@ -0,0 +1,11 @@
+/* key : value 형태로 저장하는 객체 리터럴과는 달리
+   배열 리터럴은 각 요소의 '값'만 포함한다. */
+
+var colorArr = ['orange', 'yellow', 'blue', 'green', 'red'];
+
+console.log(colorArr[0]);
+console.log(colorArr[1]);
+console.log(colorArr[4]);
+
+
+
