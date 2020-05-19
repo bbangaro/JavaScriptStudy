31a5ee5357cc975bc0e6dac34e86a6857a3a893e
밀가루 <bbangaro@gmail.com>
2020-05-19 22:32:06 +0900

    Array 공부중

--- InsideJavaScript/array.js
+++ InsideJavaScript/array.js
@@ -8,4 +8,14 @@ console.log(colorArr[1]);
 console.log(colorArr[4]);
 
 
+// 빈 배열
+var emptyArr = [];
+console.log(emptyArr[0]);
+
+// 빈 배열의 원하는 위치에 값 동적 생성
+emptyArr[0] = 100;
+emptyArr[3] = 'eight';
+emptyArr[7] = true;
+console.log(emptyArr); // 출력값 [ 100, <2 empty items>, 'eight', <3 empty items>, true ]
+console.log(emptyArr.length); // 빈 칸은 undefined 되며 길이는 가장 큰 index번호 기준으로 생성 됨
 
